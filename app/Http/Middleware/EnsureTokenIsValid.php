fc586b659c337c2ff138184d7fbb04b35767f492
BeSalt1080 <lightsoul1080@gmail.com>
2024-07-09 07:26:28 +0700

    Add middleware for osu api access token

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\HttpClientController;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Storage::missing('token.json') || time() > Storage::json('token.json')['token_expired_time']) {
+            $response = HttpClientController::GetToken();
+            $token = ['access_token' => $response['access_token'], 'token_expired_time' => time() + $response['expires_in']];
+            Storage::put('token.json', json_encode($token));
+        }
+
+        # Append token to request
+        $request->token = Storage::json('token.json')['access_token'];
+        return $next($request);
+    }
+}
