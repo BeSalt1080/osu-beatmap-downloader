fdcb5176bf0d86edc220b0744c4434465fbbecc5
BeSalt1080 <lightsoul1080@gmail.com>
2024-07-19 15:59:32 +0700

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "osu-lime": "#b3ff66",
+                "osu-pink": "#ff66ab",
+                "osu-darkorange": "#ff9966",
+                "osu-blue": "#66ccff",
+                "osu-gray": "#394246",
             },
         },
     },
