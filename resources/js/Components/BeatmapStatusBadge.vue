13bedbd6ebb7b693e92e6fac4c162ebcae31f614
BeSalt1080 <lightsoul1080@gmail.com>
2024-07-19 15:59:09 +0700

    Add beatmap status badge component

---
+++ resources/js/Components/BeatmapStatusBadge.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    status: String,
+});
+
+const statusStyle = computed(() => {
+    const statusMap = {
+        "ranked": "bg-osu-lime",
+        "approved": "bg-osu-lime",
+        "qualified": "bg-osu-blue",
+        "graveyard": "bg-black text-blue-400",
+        "pending": "bg-osu-orange",
+        "loved": "bg-osu-pink",
+    };
+
+    return statusMap[props.status] || "";
+});
+</script>
+
+<template>
+    <div :class="statusStyle" class="border rounded-xl px-2 uppercase text-osu-gray font-bold text-xs flex items-center w-fit">
+        {{ status }}
+    </div>
+</template>
